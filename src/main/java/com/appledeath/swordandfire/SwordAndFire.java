5e91e79257dacb63f518a492e4bac2d0f83f51d1
unknown <nick.gong0813@gmail.com>
2021-07-18 23:22:33 -0400

    wip

---
+++ src/main/java/com/appledeath/swordandfire/SwordAndFire.java
@@ -0,0 +1,7 @@
+package com.appledeath.swordandfire;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Utils.MOD_ID)
+public class SwordAndFire {
+}
---
+++ src/main/java/com/appledeath/swordandfire/Utils.java
@@ -0,0 +1,5 @@
+package com.appledeath.swordandfire;
+
+public class Utils {
+    public static final String MOD_ID = "swordandfire";
+}
